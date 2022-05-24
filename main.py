f48c0ee0cb58154cadd3545d9019104066624151
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-24 17:09:04 +0200

    steps until Request Body done

--- main.py
+++ main.py
@@ -1,18 +1,88 @@
 from enum import Enum
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
 
 FastAPIKalenderspruch = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
-@FastAPIKalenderspruch.get("/files/{file_path:path}")
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 5):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
-@FastAPIKalenderspruch.get("/models/{model_name}")
+@app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
@@ -22,10 +92,10 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
-@FastAPIKalenderspruch.get("/")
+@app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@FastAPIKalenderspruch.get("/items/{item_id}")
+@app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
