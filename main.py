074fdc8bbc994c45c9de1afa0cb361f90c34a325
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-25 09:22:19 +0200

    response-status-code, done

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from fastapi import FastAPI
-from typing import Union, List
-
-from pydantic import BaseModel
+from fastapi import FastAPI, status
 
 FastAPIKalenderspruch = FastAPI()
 
@@ -9,27 +6,6 @@ FastAPIKalenderspruch = FastAPI()
 
 app = FastAPI()
 
-
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: float = 10.5
-    tags: List[str] = []
-
-
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
-
-
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: str):
-    return items[item_id]
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/items/", status_code=status.HTTP_402_PAYMENT_REQUIRED)
+async def create_item(name: str):
+    return {"name": name}
