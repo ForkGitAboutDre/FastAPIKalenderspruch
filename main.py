e0c637e9d8374543943442c0f3071a828521e385
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-25 15:58:44 +0200

    Body - Updates , done

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-from typing import Set, Union
+from typing import List, Union
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
+    name: Union[str, None] = None
     description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: Set[str] = set()
+    price: Union[float, None] = None
+    tax: float = 10.5
+    tags: List[str] = []
 
 
-@app.post("/items/", response_model=Item, summary="Create an item", tags={"item"})
-async def create_item(item: Item):
-    """
-    Create an item with all the information:
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, you can omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-    return item
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
