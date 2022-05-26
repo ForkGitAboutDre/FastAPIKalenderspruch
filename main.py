f5853399d32d4ce7b4caedac36e1df26884fe2a1
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-26 21:44:16 +0200

    metadata done

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-from typing import Union
+from fastapi import FastAPI
 
-from fastapi import BackgroundTasks, Depends, FastAPI
-
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/openapi.json", docs_url=None, redoc_url=None)
 
 
-def write_log(message: str):
-    with open("log.txt", mode="a") as log:
-        log.write(message)
-
-
-def get_query(background_tasks: BackgroundTasks, q: Union[str, None] = None):
-    if q:
-        message = f"found query: {q}\n"
-        background_tasks.add_task(write_log, message)
-    return q
-
-
-@app.post("/send-notification/{email}")
-async def send_notification(
-        email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
-):
-    message = f"message to {email}\n"
-    background_tasks.add_task(write_log, message)
-    return {"message": "Message sent"}
+@app.get("/items/")
+async def read_items():
+    return [{"name": "Foo"}]
