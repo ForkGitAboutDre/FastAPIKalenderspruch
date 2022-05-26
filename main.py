c6704d7fd3b41218484d8daca0c797a196f2d929
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-26 15:39:17 +0200

    global-dependencies, done

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from fastapi import Depends, FastAPI, Header, HTTPException, Body
-
-app = FastAPI()
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 
-async def verify_token(x_token: str = Body()):
+async def verify_token(x_token: str = Header()):
     if x_token != "fake-super-secret-token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
@@ -14,6 +12,14 @@ async def verify_key(x_key: str = Header()):
     return x_key
 
 
-@app.put("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items/")
 async def read_items():
-    return [{"item": "Foo"}, {"item": "Bar"}]
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
