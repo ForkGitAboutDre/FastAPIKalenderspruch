a92fe4241d213651448b4a73fb119e545af171b8
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-26 21:35:09 +0200

    first half of backgroundtasks done

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from fastapi import Depends, FastAPI
+from fastapi import BackgroundTasks, FastAPI
 
-from dependencies import get_query_token, get_token_header
-from internal import admin
-from routers import items, users
-
-app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
 
 
-app.include_router(users.router)
-app.include_router(items.router)
-app.include_router(
-    admin.router,
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(get_token_header)],
-    responses={418: {"description": "I'm a teapot"}},
-)
+def write_notification(email: str, message=""):
+    with open("log.txt\n", mode="a") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+@app.post("/send-notification/{email}")
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "Notification sent in the background"}
