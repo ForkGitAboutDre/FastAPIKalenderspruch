835e5a32cc958b931149e0844793400ececd0734
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-27 09:24:55 +0200

    Scraping some quotes TODO: filter out non-relevant data, implement rng based on date and length of list, implement endpoint

--- main.py
+++ main.py
@@ -2,37 +2,12 @@ from typing import Union
 
 from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
-
-fake_secret_token = "coneofsilence"
-
-fake_db = {
-    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
-    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
-}
-
+from bs4 import BeautifulSoup
+import requests
 app = FastAPI()
+url = "https://de.wikiquote.org/w/index.php?title=Deutsche_Sprichw%C3%B6rter&action=edit"
+page = requests.get(url)
 
-
-class Item(BaseModel):
-    id: str
-    title: str
-    description: Union[str, None] = None
-
-
-@app.get("/items/{item_id}", response_model=Item)
-async def read_main(item_id: str, x_token: str = Header()):
-    if x_token != fake_secret_token:
-        raise HTTPException(status_code=400, detail="Invalid X-Token header")
-    if item_id not in fake_db:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return fake_db[item_id]
-
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item, x_token: str = Header()):
-    if x_token != fake_secret_token:
-        raise HTTPException(status_code=400, detail="Invalid X-Token header")
-    if item.id in fake_db:
-        raise HTTPException(status_code=400, detail="Item already exists")
-    fake_db[item.id] = item
-    return item
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id = "wpTextbox1")
+print(results.prettify())
