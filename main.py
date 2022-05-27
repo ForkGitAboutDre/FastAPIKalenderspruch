c6dee0e98bea55a61b3b9cc177bed90c538e640c
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-27 08:39:47 +0200

    metadata done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI(openapi_url="/api/v1/openapi.json", docs_url=None, redoc_url=None)
+app = FastAPI(openapi_url="/api/v1/openapi.json", redoc_url=None)
 
 
-@app.get("/items/")
-async def read_items():
-    return [{"name": "Foo"}]
+@app.get("/items/{itemid}")
+async def read_items(itemid: int):
+    return [{"name": itemid}]
