7d36274e81796a501eae80136be3e2e0a79b1570
ForkGitAboutDre <maximiliansmrcka@gmail.com>
2022-05-29 22:18:38 +0200

    updated examples for queryparams

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from utils import webscraper, quotepicker
 
 description = """
@@ -19,7 +19,7 @@ app = FastAPI(title="Kalenderspruch",
               version="1.0.0",
               contact={
                   "name": "my github account",
-                  "url": "https://github.com/ForkGitAboutDre/FastAPIKalenderspruch"
+                  "url": "https://github.com/ForkGitAboutDre"
               }
               )
 
@@ -28,7 +28,24 @@ quotes = webscraper.get_quotes()
 
 
 @app.get("/quote/")
-def read_quote(date: Union[datetime.date, None] = None):
+def read_quote(date: Union[datetime.date, None] = Query(default=None, examples={
+            "normal": {
+                "summary": "A date",
+                "description": "A date with format YYYY-MM-DD works correctly.",
+                "value": "2022-05-29"
+            },
+            "micros after epoch": {
+                "summary": "An example with an integer",
+                "description": "it automatically gets converted to a date based on milliseconds after epoch",
+                "value": 1123123121323
+            },
+            "None":{
+                "summary": "no date set.",
+                "description": "get the quote of today"
+    }
+        },
+    )
+):
     if date is None:
         return quotepicker.get_quote_of_day(quotes, datetime.date.today())
     else:
